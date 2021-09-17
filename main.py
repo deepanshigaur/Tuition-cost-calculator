8b0fce0e2129c3767bb68f2911ae5e42ffa2dd41
Deepanshi Gaur <dgaur1@binghamton.edu>
2021-09-17 14:55:14 -0400

    final commit

--- main.py
+++ main.py
@@ -3,8 +3,21 @@
 weeks = 16
 classes = 5
 tuition = 6000
+classes_per_week= 3
 cost_per_week = ((tuition / classes) / weeks)
+cost_per_class = (cost_per_week/ classes_per_week)
 print("Cost per week:", cost_per_week)
-
+print("cost per each class, you're welcome :)", cost_per_class)
+print(classes_per_week, type(classes_per_week))
+print(cost_per_week, type(cost_per_class))
 
 #Part B
+os.system ("man man")
+os.system ("man cat")
+os.system ("man pwd")
+os.system ("man mkdir")
+os.system ("man rm")
+os.system ("man ls")
+os.system ("man mv")
+os.system ("man cp")
+os.system ("man python3")
